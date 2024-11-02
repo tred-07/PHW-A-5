743f5f4f541a68d8b14ad6f1eda39b219f90d37f
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-11-03 01:04:09 +0600

    some change

--- donation.js
+++ donation.js
@@ -1,4 +1,4 @@
-let date=new Date()
+let date = new Date();
 console.log(date);
 
 document
@@ -7,7 +7,13 @@ document
     e1.preventDefault();
     let donationType = document.getElementById("donationType-1-donateAmount");
     let donationTypeNumber = parseFloat(donationType.value);
-    if (donationTypeNumber > 0) {
+    let totalDonatedAmount =
+      document.getElementById("totalDonatedAmount").innerText;
+    let totalDonatedAmountNumber = parseFloat(totalDonatedAmount);
+    if (
+      donationTypeNumber > 0 &&
+      totalDonatedAmountNumber >= donationTypeNumber
+    ) {
       let div = document.createElement("div");
       div.innerHTML = `<div class="border-[1px] border-solid border-[#222] px-[25px] py-[50px] rounded-[20px] w-[100%]">
       <p class="font-bold text-black text-[1.5rem]">${donationTypeNumber} Taka is Donated for famine-2024 at Feni, Bangladesh</p>
@@ -15,12 +21,9 @@ document
       </div>`;
       let parent = document.getElementById("donationHistoryPart");
       parent.appendChild(div);
-      div.style.width="80%"
-      div.style.marginBottom="25px"
-      let totalDonatedAmount =
-        document.getElementById("totalDonatedAmount").innerText;
-      let totalDonatedAmountNumber =
-        parseFloat(totalDonatedAmount) + donationTypeNumber;
+      div.style.width = "80%";
+      div.style.marginBottom = "25px";
+      totalDonatedAmountNumber -= donationTypeNumber;
       document.getElementById(
         "totalDonatedAmount"
       ).innerText = `${totalDonatedAmountNumber} BDT`;
@@ -33,8 +36,14 @@ document
       ).innerHTML = `<h1 id="donationType-1-totalCollection" class="text-[black] text-[1.5rem] font-bold">${
         donationType_1_totalCollection + donationTypeNumber
       } BDT</h1>`;
-      document.getElementById("donationType-1-donateAmount").value=0
+      document.getElementById("donationType-1-donateAmount").value = 0;
+      alert(`${donationTypeNumber} donated successfully.`);
+    } else if (totalDonatedAmountNumber < donationTypeNumber) {
+      document.getElementById("donationType-1-donateAmount").value = 0;
+      alert("Not enough balance");
+      console.log("Invalid value");
     } else {
+      document.getElementById("donationType-1-donateAmount").value = 0;
       alert("Please enter greater than O Bdt.");
     }
   });
@@ -46,19 +55,22 @@ document
     let donationType = document.getElementById("donationType-2-donateAmount");
     console.log(typeof parseFloat(donationType.value));
     let donationTypeNumber = parseFloat(donationType.value);
-    if (donationTypeNumber > 0) {
+    let totalDonatedAmount =
+      document.getElementById("totalDonatedAmount").innerText;
+    let totalDonatedAmountNumber = parseFloat(totalDonatedAmount);
+    if (
+      donationTypeNumber > 0 &&
+      totalDonatedAmountNumber >= donationTypeNumber
+    ) {
       let div = document.createElement("div");
       div.innerHTML = `<div class="border-[1px] border-solid border-[#222] px-[25px] py-[50px] rounded-[20px] w-[100%]"><p class="font-bold text-black text-[1.5rem] w-[80%] text-wrap">${donationTypeNumber} Taka is Donated for Flood Relief in Feni,Bangladesh</p>
       <p class="font-bold text-black text-[1rem]">Date: ${date}</p></div>`;
       let parent = document.getElementById("donationHistoryPart");
       parent.appendChild(div);
-      div.style.width="80%"
-      div.style.marginBottom="25px"
+      div.style.width = "80%";
+      div.style.marginBottom = "25px";
       console.log(donationType.value);
-      let totalDonatedAmount =
-        document.getElementById("totalDonatedAmount").innerText;
-      let totalDonatedAmountNumber =
-        parseFloat(totalDonatedAmount) + donationTypeNumber;
+      totalDonatedAmountNumber -= donationTypeNumber;
       document.getElementById(
         "totalDonatedAmount"
       ).innerText = `${totalDonatedAmountNumber} BDT`;
@@ -71,8 +83,14 @@ document
       ).innerHTML = `<h1 id="donationType-2-totalCollection" class="text-[black] text-[1.5rem] font-bold">${
         donationType_1_totalCollection + donationTypeNumber
       } BDT</h1>`;
-      document.getElementById("donationType-2-donateAmount").value=0
+      document.getElementById("donationType-2-donateAmount").value = 0;
+      alert(`${donationTypeNumber} donated successfully.`);
+    } else if (totalDonatedAmountNumber < donationTypeNumber) {
+      document.getElementById("donationType-2-donateAmount").value = 0;
+      alert("Not enough balance");
+      console.log("Invalid value");
     } else {
+      document.getElementById("donationType-2-donateAmount").value = 0;
       alert("Please enter greater than O Bdt.");
       console.log("Invalid value");
     }
@@ -85,7 +103,13 @@ document
     let donationType = document.getElementById("donationType-3-donateAmount");
     console.log(typeof parseFloat(donationType.value));
     let donationTypeNumber = parseFloat(donationType.value);
-    if (donationTypeNumber > 0) {
+    let totalDonatedAmount =
+      document.getElementById("totalDonatedAmount").innerText;
+    let totalDonatedAmountNumber = parseFloat(totalDonatedAmount);
+    if (
+      donationTypeNumber > 0 &&
+      totalDonatedAmountNumber >= donationTypeNumber
+    ) {
       let div = document.createElement("div");
       div.innerHTML = `<div class="border-[1px] border-solid border-[#222] px-[25px] py-[50px] rounded-[20px] w-[100%]">
       <p class="font-bold text-black text-[1.5rem]">${donationTypeNumber} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</p>
@@ -93,13 +117,10 @@ document
       </div>`;
       let parent = document.getElementById("donationHistoryPart");
       parent.appendChild(div);
-      div.style.width="80%"
-      div.style.marginBottom="25px"
+      div.style.width = "80%";
+      div.style.marginBottom = "25px";
       console.log(donationType.value);
-      let totalDonatedAmount =
-        document.getElementById("totalDonatedAmount").innerText;
-      let totalDonatedAmountNumber =
-        parseFloat(totalDonatedAmount) + donationTypeNumber;
+      totalDonatedAmountNumber -= donationTypeNumber;
       document.getElementById(
         "totalDonatedAmount"
       ).innerText = `${totalDonatedAmountNumber} BDT`;
@@ -112,8 +133,14 @@ document
       ).innerHTML = `<h1 id="donationType-3-totalCollection" class="text-[black] text-[1.5rem] font-bold">${
         donationType_1_totalCollection + donationTypeNumber
       } BDT</h1>`;
-      document.getElementById("donationType-3-donateAmount").value=0
+      document.getElementById("donationType-3-donateAmount").value = 0;
+      alert(`${donationTypeNumber} donated successfully.`);
+    } else if (totalDonatedAmountNumber < donationTypeNumber) {
+      document.getElementById("donationType-3-donateAmount").value = 0;
+      alert("Not enough balance");
+      console.log("Invalid value");
     } else {
+      document.getElementById("donationType-3-donateAmount").value = 0;
       alert("Please enter greater than O Bdt.");
       console.log("Invalid value");
     }
