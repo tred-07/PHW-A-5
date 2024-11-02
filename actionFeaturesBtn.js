ec5a8b02f46b7927c6988b8e85dbe21f3b87f6e8
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-11-02 13:35:34 +0600

    testing

--- actionFeaturesBtn.js
+++ actionFeaturesBtn.js
@@ -1,10 +1,12 @@
 document.getElementById("donationFeaturesBtn").addEventListener("click",function(event){
+    event.preventDefault()
     document.getElementById("donationPart").classList.remove("hidden")
     document.getElementById("donationHistoryPart").classList.add("hidden")
     document.getElementById("historyFeaturesBtn").style.background="white"
     document.getElementById("donationFeaturesBtn").style.background="#B4F461"
 })
 document.getElementById("historyFeaturesBtn").addEventListener("click",function(event){
+    event.preventDefault()
     document.getElementById("donationPart").classList.add("hidden")
     document.getElementById("donationHistoryPart").classList.remove("hidden")
     document.getElementById("historyFeaturesBtn").style.background="#B4F461"
