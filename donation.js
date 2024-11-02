0cfbc04d097b3f096b38da6ec2d7019d058125ca
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-11-02 16:48:57 +0600

    testing

--- donation.js
+++ donation.js
@@ -1,13 +1,19 @@
 document.getElementById("donationType-1-btn").addEventListener("click",(e)=>{
+    e.preventDefault()
     let donationType=document.getElementById("donationType-1-donateAmount")
     console.log(typeof(parseFloat(donationType.value)));
     let donationTypeNumber=parseFloat(donationType.value)
     if(donationTypeNumber>0){
         let p=document.createElement('p')
-        p.innerHTML=`<div><p>${donationTypeNumber} Donation Type 1</p></div>`
+        p.innerHTML=`<div><p class="font-bold text-black text-[2rem]">${donationTypeNumber} Donation Type 1</p></div>`
         let parent=document.getElementById("donationHistoryPart")
         parent.appendChild(p)
         console.log(donationType.value);
+        let totalDonatedAmount=(document.getElementById("totalDonatedAmount").innerText)
+        let totalDonatedAmountNumber=parseFloat(totalDonatedAmount)+donationTypeNumber
+        document.getElementById("totalDonatedAmount").innerText=`${totalDonatedAmountNumber} BDT`
+        console.log(typeof(totalDonatedAmount));
+        document.getElementById("donationType-1-donateAmount").innerText="0"
     }
     else{
         alert("Please enter greater than O Bdt.")
